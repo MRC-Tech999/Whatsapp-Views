16556e1ad71b3723a6a5fe5505993ebff375f1c4
MRC-Tech999 <emprerorsukuna@gmail.com>
2025-04-18 04:08:14 -0400

    Update generate_vcf.php

--- generate_vcf.php
+++ generate_vcf.php
@@ -1,12 +1,18 @@
 <?php
-$csv = file('contacts.csv', FILE_IGNORE_NEW_LINES);
-$vcf = "";
+// Lit contacts.csv et produit contact.vcf
+$csv = __DIR__ . '/contacts.csv';
+$vcf = __DIR__ . '/contact.vcf';
+if (!file_exists($csv)) return;
 
-foreach ($csv as $line) {
-  list($name, $number) = explode(',', $line);
-  $vcf .= "BEGIN:VCARD\nVERSION:3.0\nFN:$name\nTEL:$number\nEND:VCARD\n";
+$lines = file($csv, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$vcards = "";
+foreach ($lines as $i => $line) {
+  if ($i === 0) continue; // skip header
+  list($n, $num) = explode(',', $line);
+  $vcards .= "BEGIN:VCARD\r\n";
+  $vcards .= "VERSION:3.0\r\n";
+  $vcards .= "FN:$n\r\n";
+  $vcards .= "TEL;TYPE=CELL:$num\r\n";
+  $vcards .= "END:VCARD\r\n";
 }
-
-file_put_contents('contacts.vcf', $vcf);
-echo "Fichier VCF généré avec succès.";
-?>
+file_put_contents($vcf, $vcards);
